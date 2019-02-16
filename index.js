8148810dd6b52dcf1eee3f00e68cc6c3fcb145e7
Ariel <cantegone@yahoo.com>
2019-02-16 20:30:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function happyHolidaysTo(name) {
   return("Happy holidays, "+ name +"!");
 }
 
-// function happyHolidayTo(holiday, name) {
-//   return("Happy ${holiday}, ${name}!");
-// }
+function happyHolidayTo(holiday, name) {
+  return("Happy ${holiday}, ${name}!");
+}
 
 // function holidayCountdown(holiday, days) {
 //   return("It\'s ${days} days until ${holiday}!");
